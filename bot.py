41c22d62db6b28baa8535b88e18a82c0678141df
Julian Susigan <julian.susigan@etu.u-bordeaux.fr>
2025-12-04 18:11:32 +0100

    ajout de bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from chatterbot import ChatBot
+import nltk
+nltk.download('punkt_tab')
+
+chatbot = ChatBot("Chatpot")
+
+exit_conditions = (":q", "quit", "exit")
+while True:
+    query = input("> ")
+    if query in exit_conditions:
+        break
+    else:
+        print(f"🪴 {chatbot.get_response(query)}")
