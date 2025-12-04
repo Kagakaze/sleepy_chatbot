e6bedbc861a1803abb6c9023b98f1e77d825dc1f
Julian Susigan <julian.susigan@etu.u-bordeaux.fr>
2025-12-04 18:24:45 +0100

    venv added

--- bot.py
+++ bot.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
 import nltk
+
 nltk.download('punkt_tab')
 
 chatbot = ChatBot("Chatpot")
+trainer = ChatterBotCorpusTrainer(chatbot)
+trainer.train("chatterbot.corpus.english")
 
 exit_conditions = (":q", "quit", "exit")
 while True:
